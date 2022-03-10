70b4f34476cf2d54213eccd579c1afca10b82ae7
Un gars <noellor@ndlp.fr>
2022-03-10 15:43:27 +0100

    aaaaaaa

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                            <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
-<body>
-    <?php
-        function ceciESTUNMOdule() {
-            if(isset($_GET['VariablePasséeDansGet'])) {
-                $VariablePasséeDansGet = $_GET['VariablePasséeDansGet'];
-            }
-            while(true) {
+                    </head>
+                <body>
+        <?php
+                function ceciESTUNMOdule() {
+                            if(isset($_GET['VariablePasséeDansGet'])) {
+    $VariablePasséeDansGet = $_GET['VariablePasséeDansGet'];
+                                                                    }
+    while(true) {
                 echo "slt";
-            }
-        }
-    ?>
-</body>
+        echo "blep";
+                        }
+                            }
+            ?>
+                    </body>
 </html>
